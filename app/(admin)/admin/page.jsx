28836ced616ac5707e0abf2218df9a98550aad16
sagnikdey45 <deydsagnik@gmail.com>
2026-02-23 03:22:47 +0530

    Add system admin page layout coming soon banner

--- app/(admin)/admin/page.jsx
+++ app/(admin)/admin/page.jsx
@@ -4,11 +4,18 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>Admin</h1>
-      <p>Welcome to the admin dashboard!</p>
-      Coming soon...
-      
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+      <p>
+        Welcome to the admin dashboard. Here you can manage your application.
+      </p>
+      <p>This page is Coming Soon.....</p>
+      <button
+        onClick={() => (window.location.href = "/")}
+        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+      >
+        Go back home
+      </button>
     </div>
   );
 };
