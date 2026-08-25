0bb41b0f29c37de47e0e208e06e8fd8c089ca80c
kevin <kevinjasondcosta@gmail.com>
2026-08-25 23:51:53 +0530

    Add CityAdminLayout component for user authentication and role management

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import { LandingPage } from "@/components/LandingPage";
+import {useRouter } from "next/navigation";
 import React from "react";
 
 const page = () => {
+  const router = useRouter();
   return <div>
-    <LandingPage />
+    <LandingPage onGetStarted={router.push("/sign-in")}/>
   </div>;
 };
 
